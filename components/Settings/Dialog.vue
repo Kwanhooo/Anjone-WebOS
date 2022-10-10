b1e0d9f7ab9efab916b195df69bacdc8877449ee
Kwanhooo <kwanhooo@foxmail.com>
2022-10-10 17:18:06 +0800

    feat: 迁移设置组件，新增账户设定

---
+++ components/Settings/Dialog.vue
@@ -0,0 +1,125 @@
+<template>
+  <DialogBody class="dialog-settings">
+    <template #title>
+      <span>系统设置</span>
+    </template>
+    <template #side>
+      <div class="settings-catalog">
+        <div class="item-parent item active-item-parent">
+          <svg
+            t="1664884109982"
+            class="icon-user"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M642.8 531.8c64.3-42.6 106.9-115.4 106.9-198.1C749.7 202.6 643.1 96 512 96S274.3 202.6 274.3 333.7c0 82.7 42.6 155.6 106.9 198.1C215.8 582.9 96 727.7 96 898.3c0 16.4 13.3 29.7 29.7 29.7s29.7-13.3 29.7-29.7c0-180.2 159.9-326.9 356.6-326.9 196.6 0 356.6 146.6 356.6 326.9 0 16.4 13.3 29.7 29.7 29.7s29.7-13.3 29.7-29.7c0-170.6-119.8-315.4-285.2-366.5zM333.7 333.7c0-98.3 80-178.3 178.3-178.3s178.3 80 178.3 178.3S610.3 512 512 512s-178.3-80-178.3-178.3z"
+            ></path>
+          </svg>
+          <span>账户管理</span>
+          <svg
+            t="1664882277620"
+            class="icon-list"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="2476"
+          >
+            <path
+              d="M890.335385 330.911222c-12.576374-12.416396-32.800753-12.352748-45.248112 0.192662L517.248327 661.951458 184.831931 332.512727c-12.576374-12.447359-32.800753-12.352748-45.280796 0.192662-12.447359 12.576374-12.352748 32.831716 0.192662 45.280796l353.311652 350.112082c0.543583 0.543583 1.247144 0.672598 1.792447 1.183497 0.127295 0.127295 0.159978 0.287273 0.287273 0.416288 6.239161 6.175514 14.399785 9.280473 22.527725 9.280473 8.224271 0 16.479505-3.168606 22.720387-9.471415l350.112082-353.311652C902.975407 363.615643 902.880796 343.360301 890.335385 330.911222z"
+            ></path>
+          </svg>
+        </div>
+        <div class="children-wrapper">
+          <div class="item-child item active-item-child">
+            <span>我的账户</span>
+          </div>
+          <div class="item-child item">
+            <span>账户分配</span>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template #body>
+      <div id="my-account-wrapper" style="width: 100%; height: 100%">
+        <LazySettingsMyAccount />
+      </div>
+    </template>
+  </DialogBody>
+</template>
+
+<script>
+import Vue from 'vue'
+import MyAccount from '@/components/Settings/MyAccount'
+
+export default {
+  name: 'SettingsDialog',
+  data() {
+    return {
+      activeIndex: 0,
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+@import '@/assets/style/global';
+
+@CONTENT_PADDING: 0.5em 0 0.5em 3.5em;
+@INACTIVE_BACKGROUND: #f7f7f7;
+@ACTIVE_BACKGROUND: #e8e8e8;
+@TAB_FONT_COLOR: #415058;
+@ITEM_FONT_SIZE: 0.8rem;
+
+.settings-catalog {
+  font-size: @ITEM_FONT_SIZE;
+
+  .item {
+    padding: @CONTENT_PADDING;
+    cursor: pointer;
+    pointer-events: auto;
+    color: @TAB_FONT_COLOR;
+  }
+
+  .item-parent {
+    display: flex;
+    align-items: center;
+    padding-left: 1.9em !important;
+
+    svg {
+      margin-right: 0.5em;
+      fill: @TAB_FONT_COLOR;
+      width: @ITEM_FONT_SIZE;
+      height: @ITEM_FONT_SIZE;
+    }
+
+    svg.icon-list {
+      flex: 1;
+      max-width: @ITEM_FONT_SIZE;
+      min-width: @ITEM_FONT_SIZE;
+      margin: 0 1em 0 auto;
+    }
+  }
+
+  .active-item-parent {
+    color: @STRONG_THEME_COLOR_LIGHT !important;
+
+    svg {
+      fill: @STRONG_THEME_COLOR_LIGHT !important;
+    }
+  }
+
+  .children-wrapper {
+    .item-child {
+      background: @INACTIVE_BACKGROUND;
+    }
+
+    .active-item-child {
+      background: @ACTIVE_BACKGROUND !important;
+      color: @STRONG_THEME_COLOR_LIGHT !important;
+      border-right: #019e48 solid 3px;
+    }
+  }
+}
+</style>
---
+++ components/Settings/MyAccount.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="wrapper">
+    <div class="avatar-editor">
+      <img :src="avatarUrl" alt="头像" />
+    </div>
+    <div class="info-editor">
+      <span class="info-editor-key">账户昵称</span>
+      <input v-model="username" class="info-editor-value" type="text" />
+    </div>
+    <div class="info-editor">
+      <span class="info-editor-key">注册手机</span>
+      <input v-model="phone" class="info-editor-value" type="text" />
+    </div>
+    <div class="info-editor">
+      <span class="info-editor-key">登录密码</span>
+      <input v-model="password" class="info-editor-value" type="password" />
+    </div>
+    <div class="info-editor">
+      <span class="info-editor-key">填验证码</span>
+      <input v-model="captcha" class="info-editor-value" type="text" />
+    </div>
+    <div class="info-editor">
+      <span class="info-editor-key">注册时间</span>
+      <span class="info-editor-value">{{ registerTime }}</span>
+    </div>
+    <div class="info-editor">
+      <span class="info-editor-key">设备权限</span>
+      <span class="info-editor-value">{{ role }}</span>
+    </div>
+    <div class="info-editor">
+      <button class="submit" @click.prevent="onSubmitClicked()">
+        修改信息
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'MyAccount',
+  data() {
+    return {
+      avatarUrl: '',
+      username: '',
+      phone: '',
+      registerTime: '',
+      role: '',
+      password: '**********',
+      captcha: '',
+    }
+  },
+  mounted() {
+    this.initData()
+  },
+  methods: {
+    initData() {
+      this.avatarUrl = $nuxt.$store.getters['user/avatar']
+      this.username = $nuxt.$store.getters['user/username']
+      this.phone = $nuxt.$store.getters['user/phone']
+      this.registerTime = $nuxt.$store.getters['user/create_time']
+      this.role = $nuxt.$store.getters['user/role']
+    },
+    onSubmitClicked() {},
+  },
+})
+</script>
+
+<style lang="less" scoped>
+@import '@/assets/style/global';
+
+@INPUT_BORDER_COLOR: #c4c4c4;
+
+.wrapper {
+  margin: 4% 0 0 4%;
+  //background: green;
+
+  .avatar-editor {
+    height: 100px;
+    margin-bottom: 2em;
+
+    img {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+    }
+  }
+
+  .info-editor {
+    height: 50px;
+
+    .info-editor-key {
+      font-family: '黑体', sans-serif;
+      font-weight: 600;
+    }
+
+    .info-editor-value {
+      margin-left: 2.5em;
+
+      &:not(input) {
+        font-family: '黑体', sans-serif;
+        font-weight: 600;
+      }
+
+      &:not(span) {
+        border: 1px solid @INPUT_BORDER_COLOR;
+        border-radius: 8px;
+        outline: none;
+        background: transparent;
+        padding: 0.1em 0 0.1em 0.7em;
+      }
+    }
+
+    .submit {
+      background: @STRONG_THEME_COLOR_LIGHT;
+      margin-top: 0.5em;
+      padding: 0.5em 1.2em;
+      border: none;
+      color: white;
+      border-radius: 8px;
+      transition: all ease-in-out 0.15s;
+      cursor: pointer;
+      pointer-events: auto;
+      font-family: '黑体', sans-serif;
+
+      &:hover {
+        background: #148f4e !important;
+      }
+    }
+  }
+}
+</style>
