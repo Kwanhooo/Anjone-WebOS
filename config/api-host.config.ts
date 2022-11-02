136f4e91ceff0de121dfd615a757ae30a6cd0285
Kwanhooo <kwanhooo@foxmail.com>
2022-11-02 15:12:57 +0800

    build: api host config

--- config/api-host.config.ts
+++ config/api-host.config.ts
@@ -1,3 +1,2 @@
-export const xhrHost = 'https://api.anjone.0xffff.我爱你:5000'
-export const monitorWSHost = 'ws://120.78.235.195:8080'
-export const messageCenterWSHost = 'ws://120.78.235.195:8081'
+export const xhrHost = 'http://120.78.235.195:5000'
+export const monitorWSHost = 'ws://120.78.235.195:8081'
